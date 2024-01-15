4349e35dc25daa2ba9b1772787f3b80d4f0e49d0
Samuel Satizabal <samuelsatizabal@hotmail.com>
2024-01-15 00:22:29 -0500

    FEAT: endpoint for get cards

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createUser,
+  getCardsByUser,
   getUserByUsername,
   getUsers,
 } from "../controller/user.controller";
@@ -8,9 +9,8 @@ import {
 const router = Router();
 
 router.post("/user", createUser);
-
 router.get("/user", getUsers);
-
 router.get("/user/:username", getUserByUsername);
+router.get("/cardsByUser/:username", getCardsByUser);
 
 export default router;
